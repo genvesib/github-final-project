1f517b761d5a094c518075dd0bfff18993f29b70
Benjamín Genovesi <89320764+genvesib@users.noreply.github.com>
2023-05-25 18:22:12 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: genvesib
    # Additional Authors:
-   # <your GitHub username>
+   # genvesib
    # Input:
    # p, principal amount
    # t, time period in years
